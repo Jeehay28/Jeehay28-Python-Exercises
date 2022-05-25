7f643918bd272a45b09eb35ce8754fe966733aa0
Jeehay28 <106166065+Jeehay28@users.noreply.github.com>
2022-05-26 00:50:28 +0900

    [Bronze IV] Title: 사분면 고르기, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,11 @@
+x=int(input())
+y=int(input())
+if (x>0) and (y>0):
+  quadrant=1
+elif (x<0) and (y>0):
+  quadrant=2 
+elif (x<0) and (y<0):
+  quadrant=3
+elif (x>0) and (y<0):
+  quadrant=4
+print(quadrant)
