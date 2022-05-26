5d370341c32aa42fcb07a86f376ae7762455dd33
Jeehay28 <106166065+Jeehay28@users.noreply.github.com>
2022-05-27 00:31:57 +0900

    [Bronze IV] Title: 주사위 세개, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,13 @@
+a,b,c = map(int, input().split())
+
+if a==b==c:
+  prize=10000+a*1000
+
+elif (a==b or b==c):
+  prize=1000+b*100
+elif (a==c):
+  prize=1000+a*100
+else:
+  prize=(max(a,b,c))*100
+
+print(prize)
