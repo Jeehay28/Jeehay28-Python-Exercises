f05a0a7d62327e25bcd333e9b5bc9461efae0980
Jeehay28 <106166065+Jeehay28@users.noreply.github.com>
2022-06-02 00:02:58 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,6 @@
+alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+S=input()
+
+for i in range(26):
+  x=alphabet[i]
+  print(S.find(x), end=' ')
