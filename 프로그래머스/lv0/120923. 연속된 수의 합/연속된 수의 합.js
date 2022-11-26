c4e91c5cb1f4a96975a62863ba1fc60f7961e514
Jeehay Park (박지혜) <106166065+Jeehay28@users.noreply.github.com>
2022-11-27 01:06:47 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -0,0 +1,14 @@
+function solution(num, total) {
+    var answer = [];
+    let sum = 0;
+    for (let i=0; i<num; i++){
+       sum += i;
+    }
+    
+    let x = (total - sum) / num;
+    for (let i=0; i<num; i++){
+        answer.push(x+i);
+    }
+    
+    return answer;
+}
