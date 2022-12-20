79f688946ea2fbda25e5b659bb55bef20313f2d1
Jeehay Park (박지혜) <106166065+Jeehay28@users.noreply.github.com>
2022-12-20 14:13:23 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,13 @@
+function solution(strings, n) {
+    let temp = []
+    for(any of strings) {
+        temp.push(any[n] + any)
+    }
+    temp.sort();
+    let arr = []
+    for (any of temp) {
+        arr.push(any.slice(1));
+    }
+
+    return arr;
+}
