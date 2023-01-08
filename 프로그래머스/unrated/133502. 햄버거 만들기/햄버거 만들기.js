341f659277124d2d0d9033724b5d82d9151fd696
Jeehay Park (박지혜) <106166065+Jeehay28@users.noreply.github.com>
2023-01-08 09:32:53 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,13 @@
+function solution(ingredient) {
+    let str = ingredient.join("");
+    let cnt = 0;
+    let pattern = /1231/
+    while (pattern.test(str)) {
+        let left = str.slice(0, str.search(pattern));
+        left.slice(-4)
+        let right = str.slice(str.search(pattern)+4);
+        str = left.slice(-4) + str.slice(str.search(pattern)+4);
+        cnt += 1
+    }
+    return cnt;
+}
