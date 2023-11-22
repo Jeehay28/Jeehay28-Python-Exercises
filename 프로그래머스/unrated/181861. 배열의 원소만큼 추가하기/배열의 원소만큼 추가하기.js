d1d27f1e78ce8fc9ccfb27194adf362401eb2bf3
Jeehay Park (박지혜) <106166065+Jeehay28@users.noreply.github.com>
2023-11-22 23:23:25 +0900

    [level unrated] Title: 배열의 원소만큼 추가하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    
+    var answer = [];
+    
+    for (any of arr) {
+        
+        for(let i=0; i<any; i++) {
+            answer.push(any)
+        }
+        
+    }
+    
+    return answer;
+    
+}
