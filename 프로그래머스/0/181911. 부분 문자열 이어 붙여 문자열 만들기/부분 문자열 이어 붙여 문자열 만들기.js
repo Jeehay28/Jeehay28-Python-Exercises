605d25977bd96fc98e08c56aed4f165af44d5ffa
Jeehay Park (박지혜) <106166065+Jeehay28@users.noreply.github.com>
2024-02-25 13:28:28 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,9 @@
+function solution(my_strings, parts) {
+    let answer = "";
+    for(let i=0; i<my_strings.length; i++) {
+        const [start, end] = parts[i];
+        answer += my_strings[i].slice(start, end+1);
+    }
+    
+    return answer;
+}
