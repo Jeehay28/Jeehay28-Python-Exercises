9395be1e4f569a498983e154df726b2c7b72c3dd
Jeehay Park (박지혜) <106166065+Jeehay28@users.noreply.github.com>
2024-03-01 20:47:28 +0900

    [level 0] Title: 세로 읽기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -0,0 +1,11 @@
+function solution(my_string, m, c) {
+    
+    let answer = [];
+    
+    for(let i=0; i<my_string.length; i+=m) {
+        answer.push(my_string.slice(i, i+m));
+    }
+    
+    return answer.map(item => item[c-1]).join("");
+        
+}
