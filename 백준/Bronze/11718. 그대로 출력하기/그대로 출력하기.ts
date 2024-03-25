81b15589459ba1de732e2195eceb80e6b53f4208
Jeehay Park (박지혜) <106166065+Jeehay28@users.noreply.github.com>
2024-03-25 21:31:53 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 136 ms, Memory: 9496 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.ts
@@ -0,0 +1,7 @@
+let input11718 = require("fs").readFileSync(0, "utf8").trim().split("\n");
+
+const arr11718: string[] = input11718.map((str: string) => str.trim());
+
+arr11718.forEach((item) => {
+  console.log(item);
+});
