db22a722a0dd8c5987a5a8a772825ffd3d8a98a8
Jeehay Park (박지혜) <106166065+Jeehay28@users.noreply.github.com>
2024-09-29 17:13:44 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 49.09 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,18 @@
+function solution(numbers) {
+    
+    let result = Array(numbers.length).fill(-1);
+    let stack = [];
+   
+    for(let i=0; i<numbers.length; i++){
+                
+        while(stack.length > 0 && numbers[i] > numbers[stack[stack.length - 1]]) {
+            const index = stack.pop();
+            result[index] = numbers[i];
+        }
+        
+        stack.push(i);
+        
+    }
+  
+    return result;
+}
